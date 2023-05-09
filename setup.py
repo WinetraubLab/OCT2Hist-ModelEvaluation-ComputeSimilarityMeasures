0781df67f4f7c331d47216291ae458cdf23e3add
PatoRoa <patricio.ilan.gonzalez2001@gmail.com>
2023-05-09 15:06:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,131 +1,57 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Configuration for the google.colab package."""
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
+from setuptools import find_packages
+from setuptools import setup
 
-import io
-import os
-import sys
-from shutil import rmtree
+DEPENDENCIES = (
+    # Note: these dependency versions should be kept in-sync with the versions
+    # specified in the docker container requirements files.
+    'google-auth>=1.17.2',
+    'ipykernel~=5.5.6',
+    'ipython~=7.34.0',
+    'notebook==6.4.8',
+    'pandas~=1.5.3',
+    'portpicker~=1.3.1',
+    'requests>=2.27.0',
+    'tornado~=6.2.0',
+)
 
-from setuptools import find_packages, setup, Command
-
-# Package meta-data.
-NAME = ' OCT2Hist-ModelEvaluation-ComputeSimilarityMeasures'
-DESCRIPTION = 'Evaluate how close is the model to ground truth by evaluating similarity measures'
-URL = 'https://github.com/WinetraubLab/OCT2Hist-ModelEvaluation-ComputeSimilarityMeasures'
-EMAIL = 'patricio.ilan.gonzalez2001@gmail.com'
-AUTHORS = 'Yonatan Winetraub, Patricio Gonzalez'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
+    name='google-colab',
+    version='1.0.0',
+    author='Google Colaboratory team',
+    author_email='colaboratory-team@google.com',
+    description='Google Colaboratory tools',
+    long_description='Colaboratory-specific python libraries.',
+    url='https://colaboratory.research.google.com/',
+    packages=find_packages(exclude=('tests*',)),
+    install_requires=DEPENDENCIES,
+    namespace_packages=('google',),
+    license='Apache 2.0',
+    keywords='google colab ipython jupyter',
+    classifiers=(
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+        'Programming Language :: Python :: 3.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+    ),
+    include_package_data=True,
 )

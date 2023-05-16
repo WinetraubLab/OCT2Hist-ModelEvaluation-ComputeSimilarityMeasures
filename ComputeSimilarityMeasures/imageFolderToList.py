259d7303533ed2e73ad18884077e2d58c60181cb
PatoRoa <patricio.ilan.gonzalez2001@gmail.com>
2023-05-16 14:52:54 -0700

    Update ImageFolderToList.py

--- ComputeSimilarityMeasures/imageFolderToList.py
+++ ComputeSimilarityMeasures/imageFolderToList.py
@@ -15,44 +15,43 @@ Example:
   imagePair_list[i][1] - fake image path
 '''
 
-class imageFolderToList:
-  def __init__(self, path):
-    self.path = path
 
-  def folderToList(self):
-    # Make list of images
-    images_files = sorted(os.listdir(self.path))
+class ImageFolderToList:
+    def __init__(self, path):
+        self.path = path
 
-    # Throws exception if length of images array is not even
-    if len(images_files) % 2 != 0:
-      raise Exception("Uneven amount of images.", len(images_files))
+    def folder_to_list(self):
+        # Make list of images
+        images_files = sorted(os.listdir(self.path))
 
-    # Sort images into their respective folders using their suffixes
-    real_files = []
-    fake_files = []
+        # Throws exception if length of images array is not even
+        if len(images_files) % 2 != 0:
+            raise Exception("Uneven amount of images.", len(images_files))
 
-    for i in images_files:
-      if i.endswith("real_B.png"):
-        real_files.append(i)
-      elif i.endswith("fake_B.png"):
-        fake_files.append(i)
+        # Sort images into their respective folders using their suffixes
+        real_files = []
+        fake_files = []
 
-    # Places pairs into main image pair list
-    pair_list = []
+        for i in images_files:
+            if i.endswith("real_B.png"):
+                real_files.append(i)
+            elif i.endswith("fake_B.png"):
+                fake_files.append(i)
 
-    for i, j in zip(real_files, fake_files):
-      # Append image pair to pair list
-      image_pair = []
-      image_pair.append(i)
-      image_pair.append(j)
-      pair_list.append(image_pair)
+        # Places pairs into main image pair list
+        pair_list = []
 
-    # Check that prefixes are the same
-    for i in pair_list:
+        for i, j in zip(real_files, fake_files):
+            # Append image pair to pair list
+            image_pair = [i, j]
+            pair_list.append(image_pair)
 
-      if i[0][:42] == i[1][:42] and i[0][43:] != i[1][43:]:
-        continue
-      else:
-        raise Exception('Image names do not match: ', i)
+        # Check that prefixes are the same
+        for i in pair_list:
 
-    return pair_list
+            if i[0][:42] == i[1][:42] and i[0][43:] != i[1][43:]:
+                continue
+            else:
+                raise Exception('Image names do not match: ', i)
+
+        return pair_list

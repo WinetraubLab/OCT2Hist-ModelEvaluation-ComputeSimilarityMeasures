cb58684d346517e589bedd28b9dccccf58ab877e
PatoRoa <patricio.ilan.gonzalez2001@gmail.com>
2023-08-18 18:02:24 -0700

    Update calculateMetricArrays.py

--- calculateMetricArrays.py
+++ calculateMetricArrays.py
@@ -8,6 +8,9 @@ from sklearn.metrics import mean_absolute_error
 from scipy.stats import pearsonr
 import csv
 
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
 '''
 This function takes lists of images, calculates their metric scores and puts them and their results into a csv file and returns vectors of each metric score
 
@@ -54,10 +57,20 @@ def calculateMetricArrays(real_image_paths, fake_image_paths, mask_image_paths, 
         b_image = np.array(cv2.cvtColor(io.imread(images_folder_path + "/" + fake_image_paths[i]), cv2.COLOR_RGB2GRAY))
 
         if len(mask_image_paths) != 0:
-            mask_image = np.array(cv2.cvtColor(io.imread(images_folder_path + "/" + mask_image_paths[i]), cv2.COLOR_RGB2GRAY))
+            # Converts mask into binary values
+            mask_image = np.array(cv2.imread(images_folder_path + "/" + mask_image_paths[i], cv2.IMREAD_GRAYSCALE))
+            mask_image = cv2.threshold(mask_image, 128, 256, cv2.THRESH_BINARY)[1]
+
+            # Isolates the section of the images as delineated by the mask
+            a_image = a_image[np.any(mask_image != 0, axis=1)]
+            b_image = b_image[np.any(mask_image != 0, axis=1)]
+
+            window_size = np.shape(a_image)[0]
+            if window_size % 2 == 0:
+                window_size -= 1
 
         # Calculate SSIM, round it, and add to array
-        scores_ssim.append(round(structural_similarity(a_image, b_image, win_size=255), 3))
+        scores_ssim.append(round(structural_similarity(a_image, b_image, win_size=window_size), 3))
 
         # Calculate MSE, round it, and add to array
         scores_mse.append(round(mean_squared_error(a_image, b_image), 3))
